87463af749c3126e1ba8d093f262716da1e5c3c3
Victor Ribeiro de Lima <victor@DESKTOP-4VLO0U0.>
2025-06-11 12:56:53 -0300

    test: test with rsbuild too

--- test/playwright.rsbuild.config.ts
+++ test/playwright.rsbuild.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from "@playwright/test";
 export default defineConfig({
   webServer: {
     command: "npm run build:rsbuild && npm run serve:rsbuild",
-    port: 5173,
+    port: 4173,
     reuseExistingServer: !process.env.CI,
   },
   testDir: "./playwright",
--- test/playwright.vite.config.ts
+++ test/playwright.vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "@playwright/test";
 
 export default defineConfig({
   webServer: {
-    command: "npm run build && npm run serve",
+    command: "npm run build:vite && npm run serve:vite",
     port: 4173,
     reuseExistingServer: !process.env.CI,
   },
